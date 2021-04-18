53858d2aca4259a7beab06973331db1a9862f803
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-04-18 16:59:06 -0300

    Fazendo teste para deploy de coordenadas

--- app.py
+++ app.py
@@ -20,32 +20,6 @@ if __name__ == '__main__':
 
     else:
         df = gera_dataframe(bd)
-        df = agrupa_visitas(df, equipes)
-
-        try:
-            m = map_plot(df, origem)
-
-        except KeyError:
-            st.write('Não há visitas previstas nesta data.')
-
-        except AttributeError:
-            st.write('Selecione um endereço válido.')
-
-        else:
-            folium_static(m)
-            lista_rotas = calcula_distancias(df, origem)
-            grupos_end = retorna_rotas(lista_rotas)
-
-            for r, e in grupos_end.items():
-                texto = ''
-                st.markdown(f'### {r}')
-
-                for i in range(len(e)):
-                    texto += f'{e[i]} '
-
-                    if i != len(e) - 1:
-                        texto += '-> '
-
-                st.markdown(f'- {texto.strip()}.')
-
-            pprint(distancias_max_min(df, origem))
+        coord = converte_endereco(origem)
+        st.write(coord)
+        # df = agrupa_visitas(df, equipes)
